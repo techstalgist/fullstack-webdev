ce85578258365e260c29021338803795725b8016
techstalgist <mikko.i.ruuskanen@gmail.com>
2018-01-01 18:10:12 +0200

    t66

--- osa4/index.js
+++ osa4/index.js
@@ -4,19 +4,29 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
 
 app.use(cors())
 app.use(bodyParser.json())
 
-const mongoUrl =  'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useMongoClient: true })
+mongoose.connect(config.mongoUrl, { useMongoClient: true })
 mongoose.Promise = global.Promise
 
 const blogsRouter = require('./controllers/blogs')
 
 app.use('/api/blogs', blogsRouter)
 
-const PORT = 3003
-app.listen(PORT, () => {
+const PORT = config.port
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
