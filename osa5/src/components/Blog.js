289675d60cb7c8566db61d57c53f39ea5533b363
techstalgist <mikko.i.ruuskanen@gmail.com>
2018-01-06 21:01:34 +0200

    t86

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,8 +1,36 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showDetails: false
+    }
+  }
+
+  toggleDetails = () => {
+    this.setState({showDetails: !this.state.showDetails})
+  }
+
+  render() {
+    const {title, author, url, likes, user} = this.props.blog
+    const showWhenVisible = { display: this.state.showDetails ? '' : 'none' }
+
+    return (
+      <div className="blog-style">
+        <div onClick={this.toggleDetails}>{title}, author: {author}</div>
+        <div style={showWhenVisible}>
+          <div className="margin-left-sm">
+            <a href={url}>{url}</a>
+            <div>{likes} likes
+              <button className="margin-left-sm">like</button>
+            </div>
+            <div>added by {user.name}</div>
+        </div>
+        </div>
+      </div> 
+    )
+  }
+}
 
 export default Blog
