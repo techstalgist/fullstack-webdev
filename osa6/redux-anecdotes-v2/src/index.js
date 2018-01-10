9c6f8f1d80555c2251f71f029d0ada59139a3706
techstalgist <mikko.i.ruuskanen@gmail.com>
2018-01-10 18:45:15 +0200

    fix

--- osa6/redux-anecdotes-v2/src/index.js
+++ osa6/redux-anecdotes-v2/src/index.js
@@ -15,14 +15,9 @@ const reducer = combineReducers({
 
 const store = createStore(reducer)
 
-const render = () => {
-  ReactDOM.render(
-    <Provider store={store} >
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  )
-}
-
-render()
-store.subscribe(render)
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
