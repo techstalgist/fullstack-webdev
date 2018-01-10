1d781564be563cf4a3c690a67ca17241ebd88c71
techstalgist <mikko.i.ruuskanen@gmail.com>
2018-01-10 17:48:46 +0200

    t100

---
+++ osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {voteAdding} from '../reducers/anecdoteReducer'
+
+class AnecdoteList extends React.Component {
+  render() {
+    const anecdotes = this.props.store.getState()
+    return (
+      <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => this.props.store.dispatch(voteAdding(anecdote.id)) }>
+                vote
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default AnecdoteList
