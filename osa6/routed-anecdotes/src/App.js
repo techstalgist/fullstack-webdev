4d86c25fcb46602abee35bc1c41a0d2dbdff3205
techstalgist <mikko.i.ruuskanen@gmail.com>
2018-01-13 09:59:14 +0200

    t115

--- osa6/routed-anecdotes/src/App.js
+++ osa6/routed-anecdotes/src/App.js
@@ -1,94 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-
-const Menu = () => (
-  <div>    
-    <Link to="/">anecdotes</Link>&nbsp;
-    <Link to="/create">create new</Link>&nbsp;
-    <Link to="/about">about</Link>&nbsp;
-  </div>
-)
-
-const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
-    </ul>  
-  </div>
-)
-
-const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
-
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
-)
-
-const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
-
-    See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code. 
-  </div>
-)
-
-class CreateNew extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      content: '',
-      author: '',
-      info: ''
-    }
-  }
-
-  handleChange = (e) => {
-    console.log(e.target.name, e.target.value)
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.addNew({
-      content: this.state.content,
-      author: this.state.author,
-      info: this.state.info,
-      votes: 0
-    })
-  }
-
-  render() {
-    return(
-      <div>
-        <h2>create a new anecdote</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            content 
-            <input name='content' value={this.state.content} onChange={this.handleChange} />
-          </div>
-          <div>
-            author
-            <input name='author' value={this.state.author} onChange={this.handleChange} />
-          </div>
-          <div>
-            url for more info
-            <input name='info' value={this.state.info} onChange={this.handleChange} />
-          </div> 
-          <button>create</button>
-        </form>
-      </div>  
-    )
-
-  }
-}
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import About from './components/About'
+import Menu from './components/Menu'
+import AnecdoteList from './components/AnecdoteList'
+import Footer from './components/Footer'
+import CreateNew from './components/CreateNew'
+import Anecdote from './components/Anecdote'
 
 class App extends React.Component {
   constructor() {
@@ -136,7 +53,12 @@ class App extends React.Component {
     this.setState({ anecdotes })
   }
 
+  
+
   render() {
+    const anecdoteById = (id) => 
+      this.state.anecdotes.find(a => a.id === Number(id))
+    
     return (
       <div>
         <h1>Software anecdotes</h1>
@@ -146,6 +68,9 @@ class App extends React.Component {
             <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
             <Route path="/create" render={() => <CreateNew addNew={this.addNew}/>} />
             <Route path="/about" render={() => <About />} />
+            <Route exact path="/anecdotes/:id" render={({match}) => 
+              <Anecdote anecdote={anecdoteById(match.params.id)} />
+            } />
           </div>
         </Router>
         <Footer />
