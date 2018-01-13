ccf1434843101e43bd5877b3313e31c21ba549a6
techstalgist <mikko.i.ruuskanen@gmail.com>
2018-01-13 10:33:05 +0200

    t118

--- osa6/routed-anecdotes/src/components/Menu.js
+++ osa6/routed-anecdotes/src/components/Menu.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
-const Menu = () => (
-    <div>    
-      <Link to="/">anecdotes</Link>&nbsp;
-      <Link to="/create">create new</Link>&nbsp;
-      <Link to="/about">about</Link>&nbsp;
+const Menu = () => {
+  const background = {
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '15px',
+    padding: '10px',
+    marginTop: '10px',
+    marginBottom: '10px',
+    width:'40%',
+    backgroundColor: '#6fe4ffd6'
+  }
+  const active = {
+    backgroundColor: '#adb9e6'
+  }
+  return (
+    <div style={background}>    
+      <NavLink exact to="/" activeStyle={active}>anecdotes</NavLink>&nbsp;
+      <NavLink exact to="/create" activeStyle={active}>create new</NavLink>&nbsp;
+      <NavLink exact to="/about" activeStyle={active}>about</NavLink>&nbsp;
     </div>
-)
+  )
+}
 
 export default Menu
